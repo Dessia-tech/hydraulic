8e30bc2639c2bf091bf84e5b1713c7d78dc4e1c0
Steven Masfaraud <masfaraud@dessia.tech>
2019-11-06 10:58:50 +0100

    Get version enhancment in setup

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ version_re = re.compile('^Version: (.+)$', re.M)
 def readme():
     with open('README.md') as f:
         return f.read()
-    
+
 def get_version():
     # Return the version if it has been injected into the file by git-archive
     version = tag_re.search('$Format:%D$')
@@ -25,33 +25,35 @@ def get_version():
         return version.group(1)
 
     d = dirname(__file__)
-    
+
     if isdir(join(d, '.git')):
-        cmd = 'git describe --tags  --dirty'
+        cmd = 'git describe --tags'
         try:
             version = check_output(cmd.split()).decode().strip()[:]
         except CalledProcessError:
-            version = 'v0.0.0'
-#            raise RuntimeError('Unable to get version number from git tags')
+            raise RuntimeError('Unable to get version number from git tags')
         if version[0]=='v':
             version = version[1:]
-#        print(version)
         # PEP 440 compatibility
         if '-' in version:
-            if version.endswith('-dirty'):
-                version = '.dev'.join(version.split('-')[:-1][:2])+'-dirty'
-        else:
-            version = '.dev'.join(version.split('-')[:2])
+            future_version = version.split('-')[0].split('.')
+            if 'post' in future_version[-1]:
+                future_version = future_version[:-1]
+            future_version[-1] = str(int(future_version[-1])+1)
+            future_version = '.'.join(future_version)
+            number_commits = version.split('-')[1]
+            version = '{}.dev{}'.format(future_version, number_commits)
+            return version
 
     else:
         # Extract the version from the PKG-INFO file.
         with open(join(d, 'PKG-INFO')) as f:
             version = version_re.search(f.read()).group(1)
-            
-#    # Writing to file
-#    with open('powertransmission/version.py', 'w+') as vf:
-#        vf.write("# -*- coding: utf-8 -*-\nversion = '{}'".format(version))
-                 
+
+    # Writing to file
+    with open('genmechanics/version.py', 'w') as vf:
+        vf.write("# -*- coding: utf-8 -*-\nversion = '{}'".format(version))
+
     return version
 
 
