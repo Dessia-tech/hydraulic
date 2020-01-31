a8a4cd8022deca7cae48f90064f30399bfb970cf
Steven Masfaraud <masfaraud@dessia.tech>
2020-01-31 15:07:55 +0100

    Setup version enhancement (from volmdlr)

--- setup.py
+++ setup.py
@@ -18,6 +18,47 @@ def readme():
     with open('README.md') as f:
         return f.read()
 
+def version_from_git_describe(version):
+    if version[0]=='v':
+            version = version[1:]
+            
+    # PEP 440 compatibility
+    number_commits_ahead = 0
+    if '-' in version:
+        version, number_commits_ahead, commit_hash = version.split('-')
+        number_commits_ahead = int(number_commits_ahead)
+
+    split_versions = version.split('.')
+    if len(split_versions) == 4:
+        suffix = split_versions[3]
+        split_versions = split_versions[:3]
+    else:
+        suffix = None
+    
+    for suffix2 in ['a', 'b', 'rc']:
+        if suffix2 in split_versions[-1]:
+            if number_commits_ahead > 0:
+                split_versions[-1] = str(split_versions[-1].split(suffix2)[0])
+                split_versions[-1] = str(int(split_versions[-1])+1)
+                future_version = '.'.join(split_versions)
+                return '{}.dev{}'.format(future_version, number_commits_ahead)
+
+            else:
+                return '.'.join(split_versions)
+    if number_commits_ahead > 0:
+        split_versions[-1] = str(int(split_versions[-1])+1)
+        split_versions = '.'.join(split_versions)
+        return '{}.dev{}'.format(split_versions, number_commits_ahead)
+    else:
+        if suffix is not None:
+            split_versions.append(suffix)
+
+        return '.'.join(split_versions)
+    
+# Just testing if get_version works well
+for v in ['v0.1.7.post2', 'v0.0.1-25-gaf0bf53', 'v0.0.1a2-25-gaf0bf53']:
+    version_from_git_describe(v)
+    
 def get_version():
     # Return the version if it has been injected into the file by git-archive
     version = tag_re.search('$Format:%D$')
@@ -25,35 +66,22 @@ def get_version():
         return version.group(1)
 
     d = dirname(__file__)
-
+    
     if isdir(join(d, '.git')):
         cmd = 'git describe --tags'
         try:
             version = check_output(cmd.split()).decode().strip()[:]
+            
         except CalledProcessError:
             raise RuntimeError('Unable to get version number from git tags')
-        if version[0]=='v':
-            version = version[1:]
-        # PEP 440 compatibility
-        if '-' in version:
-            future_version = version.split('-')[0].split('.')
-            if 'post' in future_version[-1]:
-                future_version = future_version[:-1]
-            future_version[-1] = str(int(future_version[-1])+1)
-            future_version = '.'.join(future_version)
-            number_commits = version.split('-')[1]
-            version = '{}.dev{}'.format(future_version, number_commits)
-            return version
-
+        
+        return version_from_git_describe(version)
     else:
         # Extract the version from the PKG-INFO file.
         with open(join(d, 'PKG-INFO')) as f:
             version = version_re.search(f.read()).group(1)
-
-    # Writing to file
-    with open('genmechanics/version.py', 'w') as vf:
-        vf.write("# -*- coding: utf-8 -*-\nversion = '{}'".format(version))
-
+            
+    # print('version', version)
     return version
 
 
